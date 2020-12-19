8a9ec0c19248589e9004bad2807a74fe080762bf
louisgreenhall <62178792+louisgreenhall@users.noreply.github.com>
2020-12-19 20:12:39 +0000

    updated db to use mysql, updated requirements.txt

--- ms4/settings.py
+++ ms4/settings.py
@@ -104,8 +104,12 @@ WSGI_APPLICATION = 'ms4.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ms4',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
