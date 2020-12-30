d1248d6a424ad73428ec33f5aff3bc74e95568e9
louisgreenhall <62178792+louisgreenhall@users.noreply.github.com>
2020-12-30 19:59:52 +0000

    Updated models.py, created model for request page comments and migrated

--- home/models.py
+++ home/models.py
@@ -10,13 +10,33 @@ class Request(models.Model):
     colour = models.CharField(max_length=255, default='')
     backing = models.CharField(max_length=255, default='')
     files = models.CharField(max_length=255, default='')
+
     def get_absolute_url(self):
         return '/requests/' + str(self.id)
+
     def save(self, *args, **kwargs):
         # If I need to do anything before it saves
+        # if I need to do anything before it saves
         super(Request, self).save(*args, **kwargs)
     
     class Meta:
         ordering = ['created_on']
+
+        def __unicode__(self):
+            return self.title
+
+class Comment(models.Model):
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    author = models.TextField()
+    request_id = models.IntegerField(default=0)
+
+    def save(self, *args, **kwargs):
+        # if I need to do anything before it saves
+        super(Comment, self).save(*args, **kwargs)
+    
+    class Meta:
+        ordering = ['-created_on']
+
         def __unicode__(self):
             return self.title
