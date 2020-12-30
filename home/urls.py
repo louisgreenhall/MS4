eeb790951b5fe210acdc411355d4520470e1376b
louisgreenhall <62178792+louisgreenhall@users.noreply.github.com>
2020-12-30 16:35:05 +0000

    Updated login system to use django auth, and created views for permutations of authentication (logged in, logged out)

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
 from . import views
+
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('login', views.login, name='login'),
     path('request', views.request, name='request'),
-    path('signup', views.signup, name='signup')
+    path('signup', views.signup, name='signup'),
+    path('login', LoginView.as_view(template_name='home/login.html'), name='login', ),
+    path('logout', LogoutView.as_view(template_name='home/logged_out.html'), name='logout'),
 ]
